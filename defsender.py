b61f9af6cf38e7cf9a26ff2ab37b4691b5662e03
MiroshanToshan <abakumovmiron608@gmail.com>
2025-12-26 18:07:31 +0500

    Refactoring

---
+++ defsender.py
@@ -0,0 +1,110 @@
+import os
+from dotenv import load_dotenv
+from system.send import send_mail
+import json 
+import argparse
+import PyInstaller
+import customtkinter
+from system.settings import config
+load_dotenv()
+
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+window = customtkinter.CTk() 
+window.geometry("400x240")
+
+customtkinter.set_default_color_theme("green")
+customtkinter.set_appearance_mode("dark")
+
+
+
+def start():
+  with open("mails.json", "r", encoding="UTF-8") as mails_js:
+    mails = mails_js.read()
+
+  with open("text.txt", "r", encoding="UTF-8") as text_txt:
+    text = text_txt.read()
+  mails = json.loads(mails)
+
+  with open("config.json", "r", encoding="UTF-8") as config_js:
+    configs = config_js.read()
+  configs = json.loads(configs)
+
+  text = text.replace("%website%", "https://boulderbugle.com/-133m2lwo")
+  text= text.replace("%friend_name%", friend_name.get())
+  text = text.replace("%my_name%", configs["my_name"])
+  
+  for mail in mails:
+      send_mail(configs["my_mail"], mail, subject_field.get(),  configs["password"], text)
+
+
+
+window.title("DefSender")
+window.geometry("720x480")
+
+
+
+
+subject_field = customtkinter.CTkEntry(window, placeholder_text="")
+subject_field.insert(0, "") 
+subject_field.place(x=268, y=130)
+
+subject_field_label= customtkinter.CTkLabel(window, text="Введите тему ->", fg_color="transparent")
+subject_field_label.place(x=163, y=130)
+
+
+
+
+
+
+friend_name = customtkinter.CTkEntry(window, placeholder_text="")
+friend_name.insert(0, "") 
+friend_name.place(x=268, y=80)
+
+recipient_field_label = customtkinter.CTkLabel(window, text=
+                                              '''Введите имя получателя ->
+(Необязательно)
+                                               ''', fg_color="transparent")
+recipient_field_label.place(x=90, y=80)
+
+
+
+
+copyright = customtkinter.CTkLabel(window, text="Все права защищены Defsender 2024 ©", fg_color="transparent", font=('Times 30',11) )
+copyright.place(x=10, y=450)
+
+version = customtkinter.CTkLabel(window, text="Beta 2.1", fg_color="transparent", font=('Times 30',11) )
+version.place(x=675, y=450)
+
+support = customtkinter.CTkLabel(window, text="Написать в поддержку - defsender@yandex.ru", fg_color="transparent", font=('Times 30',13) )
+support.place(x=220, y=200)
+
+
+def checkbox_event():
+    print("Cогласие:", check_var.get())
+
+check_var = customtkinter.StringVar(value="Отклонено")
+checkbox = customtkinter.CTkCheckBox(window, text="Данные указаны верно", command=checkbox_event,
+                                     variable=check_var, onvalue="Принято", offvalue="Отклонено")
+checkbox.place(x=250, y=230)
+
+
+button = customtkinter.CTkButton(master=window, text="Начать рассылку", command=start)
+button.place(x=340,y=300, anchor=customtkinter.CENTER)
+
+
+
+button_image = customtkinter.CTkImage(Image.open("./images/cogwheel.png"), size=(30, 30))
+button = customtkinter.CTkButton(master=window, image = button_image, fg_color="transparent", text="", width=35, height=35, command=config)
+button.place(x=690 ,y=410, anchor=customtkinter.CENTER)
+
+
+logo = customtkinter.CTkLabel(window, text="DefSender", fg_color="transparent", font=('Times 30',35))
+logo.place(x=500, y=50)
+logo_description = customtkinter.CTkLabel(window, text="Приложение для рассылки", fg_color="transparent", font=('Times 30',15))
+logo_description.place(x=490, y=90)
+
+
+window.mainloop()
